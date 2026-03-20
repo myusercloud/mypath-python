51ceeca6984cca634d5f097c3a988832bf77c544
myusercloud <lucasharrizon@gmail.com>
2026-03-21 00:40:11 +0300

    functions

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print("Hello world")
+
+#functions
+def greet():
+    print("Hello from Harrizon")
+
+
+greet()
+
+def add(a, b):
+    return a  + b
+
+result = add(78, 3)
+print("The sum is:", result)
+
